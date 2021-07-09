95de582999bac76f8453d1e214ac848bec776ec7
tommy <tommydeboer4@gmail.com>
2021-07-09 15:17:03 +0200

    chore: remove TODO in __init.py__

--- src/molgenis/bbmri_eric/__init__.py
+++ src/molgenis/bbmri_eric/__init__.py
@@ -1,9 +1,10 @@
 import sys
 
 if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    # noinspection PyCompatibility
     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 else:
+    # noinspection PyUnresolvedReferences
     from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
