d85606449cade83efc52164fa7d27a7cc66626a4
tommy <tommydeboer4@gmail.com>
2021-07-27 15:45:11 +0200

    feat: add test NodeData in a pickle file to use as a pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,16 @@
-# import pytest
+import pickle
+
+import pytest
+
+from molgenis.bbmri_eric._model import NodeData
+
+
+@pytest.fixture
+def node_data() -> NodeData:
+    """
+    Returns NodeData from node_data.pkl to test with.
+    """
+    file = open("resources/node_data.pkl", "rb")
+    node_data: NodeData = pickle.load(file)
+    file.close()
+    return node_data
