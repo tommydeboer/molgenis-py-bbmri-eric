5d653674cfb87a48a5e6d644e20685938fe88397
tommy <tommydeboer4@gmail.com>
2021-07-27 15:46:39 +0200

    test: unit test Validator

---
+++ tests/test_validator.py
@@ -0,0 +1,37 @@
+from molgenis.bbmri_eric._model import NodeData
+from molgenis.bbmri_eric._printer import Printer
+from molgenis.bbmri_eric._validation import Validator
+from molgenis.bbmri_eric.errors import EricWarning
+
+
+def test_validator(node_data: NodeData):
+    validator = Validator(node_data, Printer())
+
+    warnings = validator.validate()
+
+    assert warnings == [
+        EricWarning(
+            "bbmri-eric:contactID:EU_BBMRI-ERIC in entity: eu_bbmri_eric_NO_persons "
+            "does not start with bbmri-eric:contactID:NO_"
+        ),
+        EricWarning(
+            "bbmri-eric:networkID:EU_BBMRI-ERIC:networks:COVID19 in entity: "
+            "eu_bbmri_eric_NO_networks does not start with bbmri-eric:networkID:NO_"
+        ),
+        EricWarning(
+            "bbmri-eric:networkID:EU_BBMRI-ERIC:networks:CRC-Cohort in entity: "
+            "eu_bbmri_eric_NO_networks does not start with bbmri-eric:networkID:NO_"
+        ),
+        EricWarning(
+            "bbmri-eric:networkID:EU_BBMRI-ERIC:networks:COVID19 references invalid "
+            "id: bbmri-eric:contactID:EU_BBMRI-ERIC"
+        ),
+        EricWarning(
+            "bbmri-eric:networkID:EU_BBMRI-ERIC:networks:CRC-Cohort references "
+            "invalid id: bbmri-eric:contactID:EU_BBMRI-ERIC"
+        ),
+        EricWarning(
+            "bbmri-eric:ID:NO_CoronaTrondelag references invalid id: "
+            "bbmri-eric:networkID:EU_BBMRI-ERIC:networks:COVID19"
+        ),
+    ]
