c3371ac537cb3cc6e4314a8cd04195ce41122e32
tommy <tommydeboer4@gmail.com>
2021-08-12 15:56:12 +0200

    fix: commercial bool + tests

--- src/molgenis/bbmri_eric/_enricher.py
+++ src/molgenis/bbmri_eric/_enricher.py
@@ -28,18 +28,15 @@ class Enricher:
         for collection in self.node_data.collections.rows:
 
             def is_true(row: dict, attr: str):
-                return attr in row and row[attr] is True
+                # if the value is not entered, it is also considered true
+                return attr not in row or row[attr] is True
 
             biobank_id = collection["biobank"]
             biobank = self.node_data.biobanks.rows_by_id[biobank_id]
 
-            collection["commercial_use"] = (
-                is_true(biobank, "collaboration_commercial")
-                and is_true(collection, "collaboration_commercial")
-                and is_true(collection, "sample_access_fee")
-                and is_true(collection, "image_access_fee")
-                and is_true(collection, "data_access_fee")
-            )
+            collection["commercial_use"] = is_true(
+                biobank, "collaboration_commercial"
+            ) and is_true(collection, "collaboration_commercial")
 
     def _set_national_node_code(self):
         """
